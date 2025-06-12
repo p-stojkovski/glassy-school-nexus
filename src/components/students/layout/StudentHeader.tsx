6fc4bc7264f681753c5684d9699b80ca0d22226f
Petar Stojkovski <petars089@gmail.com>
2025-06-12 15:20:36 +0200

    refactor student components into subfolders

--- src/components/students/layout/StudentHeader.tsx
+++ src/components/students/layout/StudentHeader.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { Plus } from 'lucide-react';
-import { Button } from '../ui/button';
+import { Button } from '../../ui/button';
 
 interface StudentHeaderProps {
   onAddStudent: () => void;
--- src/components/students/list/StudentTable.tsx
+++ src/components/students/list/StudentTable.tsx
@@ -1,12 +1,12 @@
 
 import React, { useState } from 'react';
 import { AlertCircle } from 'lucide-react';
-import { Button } from '../ui/button';
-import { Badge } from '../ui/badge';
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '../ui/table';
-import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from '../ui/pagination';
-import GlassCard from '../common/GlassCard';
-import { Student } from '../../store/slices/studentsSlice';
+import { Button } from '../../ui/button';
+import { Badge } from '../../ui/badge';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '../../ui/table';
+import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from '../../ui/pagination';
+import GlassCard from '../../common/GlassCard';
+import { Student } from '../../../store/slices/studentsSlice';
 
 interface StudentTableProps {
   students: Student[];
