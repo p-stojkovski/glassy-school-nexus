2d502ae6d1625b09c1d12011a1e207fc61966756
Petar Stojkovski <pstojkovski@iperformx.com>
2025-06-15 11:21:14 +0200

    fix scheduling

--- src/domains/scheduling/components/SchedulingHeader.tsx
+++ src/domains/scheduling/components/SchedulingHeader.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
-import { Button } from '../ui/button';
 import { Plus } from 'lucide-react';
+import { Button } from '@/components/ui/button';
 
 interface SchedulingHeaderProps {
   onScheduleClass: () => void;
